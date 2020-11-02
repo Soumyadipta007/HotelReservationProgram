644329837ae2ac6b794ba82cbcc843c6bc9f4861
Soumya <soumyadipta0077@gmail.com>
2020-11-02 21:47:45 +0530

    [Soumya] Add: Method to add hotels

---
+++ HotelReservationProgram/HotelReservationProgram/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HotelReservationProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Hotel Reservation System");
+            HotelSystem hotelSystem = new HotelSystem();
+            hotelSystem.AddHotel(new Hotel("Lakewood", 110));
+            hotelSystem.AddHotel(new Hotel("Bridgewood", 150));
+            hotelSystem.AddHotel(new Hotel("Ridgewood", 220));
+        }
+    }
+}
---
+++ HotelReservationProgram/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotelReservationProgram;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        HotelSystem hotelSystem = new HotelSystem();
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string hotelName = "Lakewood";
+            int ratesForRegularCustomer = 110;
+            Hotel hotel = new Hotel(hotelName, ratesForRegularCustomer);
+            hotelSystem.AddHotel(hotel);
+            Assert.AreEqual("Lakewood", hotelSystem.hotelList[0].name);
+        }
+    }
+}
